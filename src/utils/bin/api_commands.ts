5f1e1ae54e6f238ac85e17365d4c113d23f0f9af
warren-wupeng <warren.wu@visionwx.com>
2025-04-26 21:46:41 +0800

    Update config.json for LinkedIn link, modify README.md for clarity, and comment out unused API commands

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -15,10 +15,10 @@ export const projects = async (args: string[]): Promise<string> => {
     .join('\n');
 };
 
-export const quote = async (args: string[]): Promise<string> => {
-  const data = await getQuote();
-  return data.quote;
-};
+// export const quote = async (args: string[]): Promise<string> => {
+//   const data = await getQuote();
+//   return data.quote;
+// };
 
 export const readme = async (args: string[]): Promise<string> => {
   const readme = await getReadme();
@@ -29,7 +29,7 @@ export const readme = async (args: string[]): Promise<string> => {
 export const weather = async (args: string[]): Promise<string> => {
   const city = args.join('+');
   if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
+    return 'Usage: weather [city]. Example: weather shenzhen';
   }
   const weather = await getWeather(city);
   return weather;
