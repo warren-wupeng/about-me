ea61e9cf12c75d912f816f8d0a5ee6a787c98804
Warren <85160995+warren-wupeng@users.noreply.github.com>
2026-03-01 06:05:07 +0000

    fix: remove duplicate projects export from api_commands.ts (conflicts with commands.ts)

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -1,24 +1,10 @@
-// // List of commands that require API calls
+// List of commands that require API calls
 
-import { getProjects } from '../api';
-import { getQuote } from '../api';
 import { getReadme } from '../api';
 import { getWeather } from '../api';
 
-export const projects = async (args: string[]): Promise<string> => {
-  const projects = await getProjects();
-  return projects
-    .map(
-      (repo) =>
-        `${repo.name} - <a class="text-light-blue dark:text-dark-blue underline" href="${repo.html_url}" target="_blank">${repo.html_url}</a>`,
-    )
-    .join('\n');
-};
-
-// export const quote = async (args: string[]): Promise<string> => {
-//   const data = await getQuote();
-//   return data.quote;
-// };
+// projects command is defined in commands.ts (curated showcase)
+// export const projects = ...
 
 export const readme = async (args: string[]): Promise<string> => {
   const readme = await getReadme();
